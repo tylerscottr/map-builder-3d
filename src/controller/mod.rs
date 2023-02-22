897c646e58daa1b103c5b1e6b7ae27f597971e95
Tyler Scott <tyler.scott.r@gmail.com>
2023-02-22 15:23:42 -0700

    Added MIT license to applicable source code files

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,3 +1,37 @@
+//! A mod that helps with camera controls.
+//!
+//! Note: Much of the following code has been copied from `smooth_bevy_cameras` and heavily
+//! modified rotate the camera while translating the collider and mesh.
+//!
+//! The MIT license and copyright notice appears in the source code for this file since that crate
+//! uses the MIT license. However, this crate may not be licensed the same way.
+
+// ============================================================================================= //
+//                                                                                               //
+// MIT License                                                                                   //
+//                                                                                               //
+// Copyright (c) 2023 bonsairobo (modified by Tyler Scott)                                       //
+//                                                                                               //
+// Permission is hereby granted, free of charge, to any person obtaining a copy                  //
+// of this software and associated documentation files (the "Software"), to deal                 //
+// in the Software without restriction, including without limitation the rights                  //
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                     //
+// copies of the Software, and to permit persons to whom the Software is                         //
+// furnished to do so, subject to the following conditions:                                      //
+//                                                                                               //
+// The above copyright notice and this permission notice shall be included in all                //
+// copies or substantial portions of the Software.                                               //
+//                                                                                               //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                    //
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                      //
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                   //
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                        //
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                 //
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE                 //
+// SOFTWARE.                                                                                     //
+//                                                                                               //
+// ============================================================================================= //
+
 /// A mod that creates a controller that acts like a first-person shooter.
 pub mod fps_controller;
 
